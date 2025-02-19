a9bb0e820416020a5a196d3f0c61149fd88e59a2
iuritorres <iuri.t1000@gmail.com>
2025-02-18 23:08:12 -0300

    fix: make SimplifiedManagement section a grid rather than flex container

--- src/sections/SimplifiedManagement.tsx
+++ src/sections/SimplifiedManagement.tsx
@@ -1,19 +1,19 @@
-import { simplifiedManagementData } from '@/constants';
-import Image from 'next/image';
+import { simplifiedManagementData } from "@/constants";
+import Image from "next/image";
 
-import groups from '../../public/icons/groups.svg';
-import playCircle from '../../public/icons/play_circle.svg';
-import school from '../../public/icons/school.svg';
-import teacherWithHerStudents from '../../public/images/teacher-with-her-students.webp';
+import groups from "../../public/icons/groups.svg";
+import playCircle from "../../public/icons/play_circle.svg";
+import school from "../../public/icons/school.svg";
+import teacherWithHerStudents from "../../public/images/teacher-with-her-students.webp";
 
 export function SimplifiedManagement() {
   return (
-    <section className='max-container flex max-md:flex-col justify-between items-center'>
-      <div className='flex-1'>
-        <span className='subtitle-text'>Nosso Software</span>
-        <h2 className='title-text'>Gestão Simplificada</h2>
+    <section className="max-container py-5 grid grid-cols-2 max-lg:grid-cols-1">
+      <div className="col-span-1">
+        <span className="subtitle-text">Nosso Software</span>
+        <h2 className="title-text">Gestão Simplificada</h2>
 
-        <p className='info-text pr-9 mt-7'>
+        <p className="info-text pr-9 mt-7">
           Nosso software de gestão educacional oferece um conjunto abrangente de
           funcionalidades para atender às necessidades específicas das
           instituições de ensino. Gerencie alunos, turmas, matrículas, notas,
@@ -21,53 +21,52 @@ export function SimplifiedManagement() {
           plataforma fácil de usar.
         </p>
 
-        <div className='flex flex-col sm:flex-row sm:items-center gap-5 sm:gap-10 my-8 text-primary-dark'>
-          <div className='text-3xl flex gap-4'>
-            <Image src={school} alt='school' width={44} height={44} />
+        <div className="flex flex-col sm:flex-row sm:items-center gap-5 sm:gap-10 my-8 text-primary-dark">
+          <div className="text-3xl flex gap-4">
+            <Image src={school} alt="school" width={44} height={44} />
             <div>
-              <span className='font-bold'>
+              <span className="font-bold">
                 {simplifiedManagementData.customers}+
               </span>
-              <p className='info-text'>Clientes</p>
+              <p className="info-text">Clientes</p>
             </div>
           </div>
 
-          <div className='text-3xl flex gap-4'>
-            <Image src={groups} alt='groups' width={44} height={44} />
+          <div className="text-3xl flex gap-4">
+            <Image src={groups} alt="groups" width={44} height={44} />
             <div>
-              <span className='font-bold'>
+              <span className="font-bold">
                 {simplifiedManagementData.developers}+
               </span>
-              <p className='info-text'>Desenvolvedores</p>
+              <p className="info-text">Desenvolvedores</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className='flex-1'>
-        <div className='relative hover:brightness-75 transition-all cursor-pointer'>
-          <Image
-            src={teacherWithHerStudents}
-            alt='teacher with her students'
-            className='media max-md:h-[300px] h-[380px] object-cover'
-            placeholder='blur'
-            width={615}
-            height={380}
-          />
-          {/* <span
+      <div className="col-span-1 relative hover:brightness-75 transition-all cursor-pointer">
+        <Image
+          src={teacherWithHerStudents}
+          alt="teacher with her students"
+          className="media max-md:h-[300px] h-[380px] object-cover justify-self-center"
+          placeholder="blur"
+          width={615}
+          height={380}
+          quality={100}
+        />
+        {/* <span
             className='material-symbols-rounded text-8xl text-primary-dark
             absolute left-1/2 -translate-x-[50%] top-1/2 -translate-y-[50%]'
           >
             play_circle
           </span> */}
-          <Image
-            src={playCircle}
-            alt='play circle'
-            width={96}
-            height={96}
-            className='absolute left-1/2 -translate-x-[50%] top-1/2 -translate-y-[50%]'
-          />
-        </div>
+        <Image
+          src={playCircle}
+          alt="play circle"
+          width={96}
+          height={96}
+          className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+        />
       </div>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,34 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./src/**/**/*.{js,ts,jsx,tsx,mdx}'],
-  darkMode: 'class',
+  content: ["./src/**/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
   theme: {
     fontSize: {
-      xs: ['12px', '16px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '19.5px'],
-      lg: ['18px', '21.94px'],
-      xl: ['20px', '24.38px'],
-      '2xl': ['24px', '29.26px'],
-      '3xl': ['28px', '50px'],
-      '4xl': ['48px', '58px'],
-      '8xl': ['96px', '106px'],
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["96px", "106px"],
     },
     colors: {
-      white: '#ffffff',
-      light: '#f0f8ff',
-      light2: '#e3eaf0',
-      'dark-gray': '#333333',
-      'dark-gray2': '#212529',
-      'dark-background': '#1D2226',
-      'light-gray': '#5e666d',
-      primary: '#610bd9',
-      'primary-dark': '#5617ae',
-      secondary: '#d629d9',
+      white: "#ffffff",
+      light: "#f0f8ff",
+      light2: "#e3eaf0",
+      "dark-gray": "#333333",
+      "dark-gray2": "#212529",
+      "dark-background": "#1D2226",
+      "light-gray": "#5e666d",
+      primary: "#610bd9",
+      "primary-dark": "#5617ae",
+      secondary: "#d629d9",
       // blog colors
-      dark: '#1b1b1b',
-      gray: '#747474',
+      dark: "#1b1b1b",
+      gray: "#747474",
     },
     extend: {
       backgroundImage: {
@@ -38,21 +38,21 @@ const config: Config = {
           "url('/images/university-students-throwing-their-caps-up.jpg')",
       },
       boxShadow: {
-        'primary-shadow':
-          '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        "primary-shadow":
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
       },
       borderRadius: {
-        primary: '4px',
+        primary: "4px",
       },
       fontFamily: {
-        montserrat: ['var(--font-montserrat)'],
-        poppins: ['var(--font-poppins)'],
+        montserrat: ["var(--font-montserrat)"],
+        poppins: ["var(--font-poppins)"],
       },
       screens: {
-        wide: '1440px',
+        wide: "1440px",
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
